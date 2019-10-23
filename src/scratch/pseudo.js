fcdfa060e927f031b26672370b0f0ae3c8e4599c
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-10-23 21:02:04 +0200

    added simple methods to classes; created users

--- src/scratch/pseudo.js
+++ src/scratch/pseudo.js
@@ -10,18 +10,56 @@ const User = class {
     this.recipes = [];
   }
   
+  addRecipe( newRecipe ) {
+    this.recipes.push( newRecipe );
+  }
+  
+  updateRecipe( editedRecipe ) {
+    let index = this.recipes.indexOf( editedRecipe );
+    this.recipes.splice( index, 1, editedRecipe );
+  }
+  
+  deleteRecipe( toDelete ) {
+    let index = this.recipes.indexOf( toDelete );
+    this.recipes.splice( index, 1 );
+  }
+  
+  setEmail( newEmail ) {
+    this.email = newEmail;
+  }
+  
+  setUserName( newName ) {
+    this.name = newName;
+  }
+  
+  setPassword( newPassword ) {
+    this.password = newPassword;
+  }
 };
 
 const Recipe = class {
-  constructor( title, cookingTime, ingredients, instructions, notes ) {
+  constructor( title, instructions, notes ) {
     this.title = title;
-    this.cookingTime = cookingTime;
-    this.ingredients = ingredients;
+    this.cookingTime = 0;
+    this.ingredients = [];
     this.instructions = instructions;
     this.notes = notes;
     
     // this.version = 1;
   }
+  
+  addIngredient( amount, unit, name ) {
+    this.ingredients.push( new Ingredient( amount, unit, name ) );
+  }
+  
+  setCookingTime( time ) {
+    this.cookingTime = time;
+  }
+  
+  deleteIngredient( toDelete ) {
+  
+  }
+  
 };
 
 const Ingredient = class {
@@ -31,3 +69,10 @@ const Ingredient = class {
     this.name = name;
   }
 };
+
+
+let user1 = new User( 'Anton', 'Anton@mail.com', '1234' );
+let user2 = new User( 'Berta', 'Berta@mail.com', 'qwerty' );
+let user3 = new User( 'Chris', 'Chris@mail.com', 'qwertz' );
+
+console.log( user1, user2, user3 );
