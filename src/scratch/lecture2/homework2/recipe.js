b917002155eaaff9b5e86bf657c3ceaa9f65fd7a
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-10-28 15:18:25 +0100

    removed comments

--- src/scratch/lecture2/homework2/recipe.js
+++ src/scratch/lecture2/homework2/recipe.js
@@ -22,10 +22,6 @@ const Recipe = class {
     this.versions.push(new RecipeVersion(newVersion, id));
   }
 
-  getNextVersionId() {
-    return this.versions[this.versions.length - 1].id + 1;
-  };
-
   getVersion(versionNo) {
     return this.versions[versionNo - 1];
   }
@@ -39,36 +35,9 @@ const Recipe = class {
     return this.versions.map(version => version.id).indexOf(id);
   }
 
-  // getIngredientIndexById(id) {
-  //   return this.versions.ingredients.map(ingredient => ingredient.id).indexOf(id);
-  // }
-  //
-  // getNextIngredientId(versionId) {
-  //   const currentVersion = this.versions[this.getVersionIndexById(versionId)];
-  //   const arrLength = currentVersion.ingredients.length;
-  //
-  //   return arrLength === 0 ? 1 : currentVersion.ingredients[arrLength - 1].id + 1;
-  // };
-  //
-  // addIngredient(versionId, ingredient) {
-  //   let index = this.getVersionIndexById(versionId);
-  //
-  //   index < 0
-  //     ? console.log('index out of bounds')
-  //     : this.versions[index].ingredients.push(new Ingredient(ingredient, this.getNextIngredientId(versionId)));
-  // }
-
-  // deleteIngredient(versionId, ingredientId) {
-  //   const versionIndex = this.getVersionIndexById(versionId);
-  //   const ingredientIndex = this.getIngredientIndexById(ingredientId);
-  //   let targetIngredientsArray = this.versions[versionIndex].ingredients;
-  //
-  //   targetIngredientsArray = targetIngredientsArray.filter((ingredient) => ingredient.id !== ingredientIndex);
-  // }
-  //
-  // setIngredients(versionId, ingredients) {
-  //   this.versions[this.getVersionIndexById(versionId)].setIngredients(ingredients);
-  // }
+  getNextVersionId() {
+    return this.versions[this.versions.length - 1].id + 1;
+  };
 
 };
 
