bcf74e697552a607cc8287fdeac83974d31de027
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-10-28 08:56:16 +0100

    added getter

--- src/scratch/lecture2/homework2/recipeVersion.js
+++ src/scratch/lecture2/homework2/recipeVersion.js
@@ -18,6 +18,34 @@ const RecipeVersion = class {
     this.notes = notes;
     this.tags = tags;
   }
+
+  get() {
+    return this;
+  }
+
+  // setVersion({
+  //              cookingTime,
+  //              servingSize,
+  //              ingredients,
+  //              instructions,
+  //              notes,
+  //              tags,
+  //            }) {
+  //   this.cookingTime = cookingTime;
+  //   this.servingSize = servingSize;
+  //   this.ingredients = ingredients;
+  //   this.instructions = instructions;
+  //   this.notes = notes;
+  //   this.tags = tags;
+  // };
+  //
+  // setIngredients(ingredients) {
+  //   this.ingredients = ingredients;
+  // }
+  //
+  // getIngredientIndexById(id) {
+  //   return this.ingredients.map(ingredient => ingredient.id).indexOf(id);
+  // }
 };
 
 module.exports = RecipeVersion;
