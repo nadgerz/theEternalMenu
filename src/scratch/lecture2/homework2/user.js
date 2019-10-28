18ee9b28ded788beaca2e12bbde7fad265b1a294
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-10-28 09:30:13 +0100

    added methods tu user class

--- src/scratch/lecture2/homework2/user.js
+++ src/scratch/lecture2/homework2/user.js
@@ -1,3 +1,5 @@
+const Recipe = require('./recipe');
+
 const User = class {
   constructor(name, email, password) {
     this.id = `user-${name.replace(' ', '').toLowerCase()}`; // TODO: UUID
@@ -13,19 +15,27 @@ const User = class {
     this.recipes = [];
   }
 
-  addRecipe(newRecipe) {
-    // TODO: check recipe name - NO DUPLICATES
-    this.recipes.push(newRecipe);
+  getRecipeIndexById(id) {
+    return this.recipes.map(recipe => recipe.id).indexOf(id);
   }
 
-  updateRecipe(editedRecipe) {
-    let index = this.recipes.indexOf(editedRecipe);
-    this.recipes.splice(index, 1, editedRecipe);
+  getNextRecipeId() {
+    return this.recipes[this.recipes.length - 1].id + 1;
   }
 
-  deleteRecipe(toDelete) {
-    let index = this.recipes.indexOf(toDelete);
-    this.recipes.splice(index, 1);
+  addRecipe(title, details) {
+    this.recipes.map(recipe => recipe.title).includes(title)
+      ? console.log('no duplicated recipe title allowed')
+      : this.recipes.add(new Recipe(title, details, this.getNextRecipeId()));
+  }
+
+  deleteRecipeById(id) {
+    this.recipes = this.recipes.filter(recipe => recipe.id !== id);
+  }
+
+  getRecipeById(id) {
+    let index = this.getRecipeIndexById(id);
+    return this.recipes[index];
   }
 
   setEmail(newEmail) {
