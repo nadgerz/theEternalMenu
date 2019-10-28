d2188cd1cef8606698ffbebfb08ad8bac9bb6f81
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-10-28 10:19:58 +0100

    added database, tested saving my user objects

--- src/scratch/lecture2/homework2/user_test.js
+++ src/scratch/lecture2/homework2/user_test.js
@@ -1,15 +1,19 @@
 const User = require('./user');
 const mockRecipes = require('./recipes');
 let [recipe1, recipe2, recipe3] = mockRecipes;
+const Database = require('./data/database');
+
 
 // TESTS
-// addRecipe:
-// getRecipeById:
-// deleteRecipeById:
+// addRecipe: ok
+// getRecipeById: ok
+// deleteRecipeById: ok
 
-let user1 = new User('Antonia', 'Antonia@mail.com', '1234');
-let user2 = new User('Berta', 'Berta@mail.com', 'qwerty');
-let user3 = new User('Chris', 'Chris@mail.com', 'qwertz');
+// let user1 = new User('Antonia', 'Antonia@mail.com', '1234');
+// let user2 = new User('Berta', 'Berta@mail.com', 'qwerty');
+// let user3 = new User('Chris', 'Chris@mail.com', 'qwertz');
+
+// let users = [user1, user2, user3];
 
 // console.log(user1, user2, user3);
 
@@ -17,15 +21,29 @@ let user3 = new User('Chris', 'Chris@mail.com', 'qwertz');
 // console.log('======================================');
 // console.log(recipe1.versions[0]);
 // user1.addRecipe(recipe1.title, recipe1.versions[0]);
-console.log(user1.recipes);
+// console.log(user1.recipes);
+
+// console.log('======================================');
+// mockRecipes.forEach(recipe => user1.addRecipe(recipe.title, recipe.versions[0]));
+// console.log(user1.recipes);
+
+// deleteRecipeById
+// console.log('======================================');
+// user1.deleteRecipeById(1);
+// console.log(user1.recipes);
+
+// getRecipeById
+// console.log('======================================');
+// console.log(user1.getRecipeById(1));
 
 
+// save
 console.log('======================================');
-mockRecipes.forEach(recipe => user1.addRecipe(recipe.title, recipe.versions[0]));
+// mockRecipes.forEach(recipe => user2.addRecipe(recipe.title, recipe.versions[0]));
+// console.log(users);
 
-console.log(user1.recipes);
-
-
+// Database.save('users', users);
+console.log(Database.load('users'));
 
 
 
