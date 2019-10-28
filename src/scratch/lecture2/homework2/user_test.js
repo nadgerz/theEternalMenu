93d560adb3bb76322d97ed9b059cec8838494897
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-10-28 09:56:38 +0100

    tested user.addRecipe()

--- src/scratch/lecture2/homework2/user_test.js
+++ src/scratch/lecture2/homework2/user_test.js
@@ -1,7 +1,32 @@
 const User = require('./user');
+const mockRecipes = require('./recipes');
+let [recipe1, recipe2, recipe3] = mockRecipes;
+
+// TESTS
+// addRecipe:
+// getRecipeById:
+// deleteRecipeById:
 
 let user1 = new User('Antonia', 'Antonia@mail.com', '1234');
 let user2 = new User('Berta', 'Berta@mail.com', 'qwerty');
 let user3 = new User('Chris', 'Chris@mail.com', 'qwertz');
 
-console.log(user1, user2, user3);
+// console.log(user1, user2, user3);
+
+// addRecipe
+// console.log('======================================');
+// console.log(recipe1.versions[0]);
+// user1.addRecipe(recipe1.title, recipe1.versions[0]);
+console.log(user1.recipes);
+
+
+console.log('======================================');
+mockRecipes.forEach(recipe => user1.addRecipe(recipe.title, recipe.versions[0]));
+
+console.log(user1.recipes);
+
+
+
+
+
+
