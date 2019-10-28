8744284cc2a7edea41610a4c9c641114f6254d3d
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-10-28 08:57:20 +0100

    realized that I can massiveley decrease complexity; removed no longer needed functions

--- src/scratch/lecture2/homework2/recipe.js
+++ src/scratch/lecture2/homework2/recipe.js
@@ -17,7 +17,6 @@ const Recipe = class {
     this.title = newTitle;
   }
 
-  //templateCopy : use in different function, to FILL form fields from
   addVersion(newVersion) {
     let id = this.getNextVersionId();
     this.versions.push(new RecipeVersion(newVersion, id));
@@ -40,52 +39,37 @@ const Recipe = class {
     return this.versions.map(version => version.id).indexOf(id);
   }
 
-  getIngredientIndexById(id) {
-    return this.versions.ingredients.map(ingredient => ingredient.id).indexOf(id);
-  }
+  // getIngredientIndexById(id) {
+  //   return this.versions.ingredients.map(ingredient => ingredient.id).indexOf(id);
+  // }
+  //
+  // getNextIngredientId(versionId) {
+  //   const currentVersion = this.versions[this.getVersionIndexById(versionId)];
+  //   const arrLength = currentVersion.ingredients.length;
+  //
+  //   return arrLength === 0 ? 1 : currentVersion.ingredients[arrLength - 1].id + 1;
+  // };
+  //
+  // addIngredient(versionId, ingredient) {
+  //   let index = this.getVersionIndexById(versionId);
+  //
+  //   index < 0
+  //     ? console.log('index out of bounds')
+  //     : this.versions[index].ingredients.push(new Ingredient(ingredient, this.getNextIngredientId(versionId)));
+  // }
 
-  getNextIngredientId(versionId) {
-    const currentVersion = this.versions[this.getVersionIndexById(versionId)];
-    const arrLength = currentVersion.ingredients.length;
+  // deleteIngredient(versionId, ingredientId) {
+  //   const versionIndex = this.getVersionIndexById(versionId);
+  //   const ingredientIndex = this.getIngredientIndexById(ingredientId);
+  //   let targetIngredientsArray = this.versions[versionIndex].ingredients;
+  //
+  //   targetIngredientsArray = targetIngredientsArray.filter((ingredient) => ingredient.id !== ingredientIndex);
+  // }
+  //
+  // setIngredients(versionId, ingredients) {
+  //   this.versions[this.getVersionIndexById(versionId)].setIngredients(ingredients);
+  // }
 
-    return arrLength === 0 ? 1 : currentVersion.ingredients[arrLength - 1].id + 1;
-  };
-
-  addIngredient(versionId, ingredient) {
-    let index = this.getVersionIndexById(versionId);
-
-    index < 0
-      ? console.log('index out of bounds')
-      : this.versions[index].ingredients.push(new Ingredient(ingredient, this.getNextIngredientId(versionId)));
-  }
-
-  deleteIngredient(versionId, ingredientId) {
-    const versionIndex = this.getVersionIndexById(versionId);
-    const ingredientIndex = this.getIngredientIndexById(ingredientId);
-    let targetIngredientsArray = this.versions[versionIndex].ingredients;
-
-    targetIngredientsArray = targetIngredientsArray.filter((ingredient) => ingredient.id !== ingredientIndex);
-  }
-
-  addIngredients(ingredients, versionId) {
-    // push?
-    this.versions[this.getVersionIndexById(versionId)].ingredients = [ingredients];
-  }
-
-  setCookingTime(version, time) {
-    let index = this.getVersionIndexById(version);
-    this.versions[index].cookingTime = time;
-  }
-
-  addInstructions(version, instructions) {
-    let index = this.getVersionIndexById(version);
-    this.versions[index].instructions = instructions;
-  }
-
-  addNotes(version, note) {
-    let index = this.getVersionIndexById(version);
-    this.versions[index].notes = note;
-  }
 };
 
 module.exports = Recipe;
