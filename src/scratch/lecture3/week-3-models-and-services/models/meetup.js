754a3da5f7017e61e497e3e24e3fba56a6c5c512
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-10-31 15:23:25 +0100

    ran format

--- src/scratch/lecture3/week-3-models-and-services/models/meetup.js
+++ src/scratch/lecture3/week-3-models-and-services/models/meetup.js
@@ -1,18 +1,24 @@
-const chalk = require('chalk')
+const chalk = require('chalk');
 
 module.exports = class Meetup {
-    constructor(name, location, attendees = [], id) {
-        this.name = name
-        this.location = location
-        this.attendees = attendees
-        this.id = id
-    }
+  constructor(name, location, attendees = [], id) {
+    this.name = name;
+    this.location = location;
+    this.attendees = attendees;
+    this.id = id;
+  }
 
-    report() {
-        console.log(chalk.blue.bgRed.bold(this.name), 'meetup is held at', chalk.green(this.location), 'and number of attendees are', this.attendees.length)
-    }
+  report() {
+    console.log(
+      chalk.blue.bgRed.bold(this.name),
+      'meetup is held at',
+      chalk.green(this.location),
+      'and number of attendees are',
+      this.attendees.length,
+    );
+  }
 
-    static create({ name, location, attendees, id }) {
-        return new Meetup(name, location, attendees, id)
-    }
-}
+  static create({ name, location, attendees, id }) {
+    return new Meetup(name, location, attendees, id);
+  }
+};
