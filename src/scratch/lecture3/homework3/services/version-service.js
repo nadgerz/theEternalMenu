697662cac7291ee81b0aef95c4f15298d5ffe312
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-01 15:44:24 +0100

    initial service setup

---
+++ src/scratch/lecture3/homework3/services/version-service.js
@@ -0,0 +1,12 @@
+const BaseService = require('./base-service');
+const VersionModel = require('../models/version');
+
+class VersionService extends BaseService {
+  // constructor() {
+  //   // super(VersionModel, `${__dirname}/../user-database.json`);
+  // }
+
+
+}
+
+module.exports = new VersionService();
