4ba50c79e1e097c6b1d6602909f24e2b7df849ec
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-01 17:19:40 +0100

    using uuid for id now

--- src/scratch/lecture3/homework3/models/ingredient.js
+++ src/scratch/lecture3/homework3/models/ingredient.js
@@ -1,6 +1,8 @@
+const uuidv1 = require('uuid/v1');
+
 const Ingredient = class {
   constructor({ amount = 0.0, unit = '', name, sub = [] }, id) {
-    this.id = id;
+    this.id = id || uuidv1();
     this.amount = amount;
     this.unit = unit;
     this.name = name;
