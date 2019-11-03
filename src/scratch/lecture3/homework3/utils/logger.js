31207a9a2262d9932f94ead3c648b72f3aa83f5c
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-11-04 00:11:20 +0100

    start of some utils

---
+++ src/scratch/lecture3/homework3/utils/logger.js
@@ -0,0 +1,53 @@
+let debug = false;
+debug = true;
+
+/*
+const log = stuff => {
+  if (debug) {
+	if (stuff) {
+		console.log(stuff)
+	}
+	else {
+		console.log()
+	}
+  }
+}
+*/
+
+function logger(strings, ...values) {
+  if (!debug) {
+    return;
+  }
+
+  var str = '';
+
+  for (let i = 0; i < strings.length; i++) {
+    if (i > 0) {
+      if (values[i - 1] && typeof values[i - 1] === 'object') {
+        if (values[i - 1] instanceof Error) {
+          if (values[i - 1].stack) {
+            str += values[i - 1].stack;
+            continue;
+          }
+        } else {
+          try {
+            str += JSON.stringify(values[i - 1]);
+            continue;
+          } catch (err) {}
+        }
+      }
+
+      str += values[i - 1];
+    }
+
+    str += strings[i];
+  }
+
+  console.log(str);
+
+  return str;
+}
+
+const log = stuff => (debug ? console.log(stuff) : null);
+
+module.exports = logger;
