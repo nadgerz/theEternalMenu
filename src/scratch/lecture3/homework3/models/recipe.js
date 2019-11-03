4565be9ff226284f8f20acddbfe3856a92ac1206
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-03 18:14:17 +0100

    to make static create work, removed 'version' from constructor argument list

--- src/scratch/lecture3/homework3/models/recipe.js
+++ src/scratch/lecture3/homework3/models/recipe.js
@@ -1,25 +1,23 @@
-const uuidv1 = require('uuid/v1');
+const uuid = require('uuid/v1');
 const Version = require('./version');
 
 module.exports = class Recipe {
-  constructor( title, id, dateCreated, versions = []) {
+  constructor(title, id, dateCreated) {
     this.title = title;
-    // TODO: fix below
-    // this.versions = details.map(versionDetails => new Version(versionDetails, versionDetails.id));
-    // this.versions = versions.map(version => (new Version(version, this.versions.length + 1)));
-    this.versions = versions;
-
-    // const allItems =  findAll();
-    // const lastItem = allItems[allItems.length - 1];
-    // const lastItemsId = lastItem && lastItem.id || 0;
-    // item.id = lastItemsId + 1;
-
-    this.id = id || `${this.title.replace(' ', '').toLowerCase()}-${uuidv1()}`;
-
+    this.id = id || `${this.title.replace(' ', '').toLowerCase()}-${uuid()}`;
     this.dateCreated = dateCreated || new Date;
+    this.versions = [];
 
-    // TODO: limit versions array to... 15?
     // this.images = [] // TODO: implement an images array
+  }
+
+  addVersion(version) {
+    const allItems = this.versions;
+    const lastItem = allItems[allItems.length - 1];
+    const lastItemsId = (lastItem && lastItem.id) || 0;
+    // version.id = lastItemsId + 1;
+
+    this.versions.push(new Version(version, (lastItemsId + 1)));
   }
 
   static create({ title, id, dateCreated, versions }) {
@@ -30,28 +28,3 @@ module.exports = class Recipe {
     return recipe;
   }
 };
-
-
-//
-// module.exports = class Meetup {
-//   constructor(name, location, attendees = [], id) {
-//     this.name = name;
-//     this.location = location;
-//     this.attendees = attendees;
-//     this.id = id;
-//   }
-//
-//   report() {
-//     console.log(
-//       chalk.blue.bgRed.bold(this.name),
-//       'meetup is held at',
-//       chalk.green(this.location),
-//       'and number of attendees are',
-//       this.attendees.length,
-//     );
-//   }
-//
-//   static create({ name, location, attendees, id }) {
-//     return new Meetup(name, location, attendees, id);
-//   }
-// };
