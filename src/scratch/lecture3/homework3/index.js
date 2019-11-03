7cfdd8b4ca6a52c2f9af82a6bb8e6085f65cda48
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-03 21:02:22 +0100

    testing

--- src/scratch/lecture3/homework3/index.js
+++ src/scratch/lecture3/homework3/index.js
@@ -1,23 +1,33 @@
 const User = require('./models/user');
-// const Recipe = require('./models/recipe');
+const Recipe = require('./models/recipe');
 // const RecipeService = require('./services/recipe-service');
 const UserService = require('./services/user-service');
-// let mockRecipes = require('./recipes');
+let mockRecipes = require('./models/recipes');
 
 
 console.log('The Eternal Menu!');
 console.log('===========================');
 
 async function main() {
-  // const mert = new Person('Mert', 34);
-  // const armagan = new Person('Armagan', 35);
 
   let user1 = new User('Antonia', 'Antonia@mail.com', '1234');
   let user2 = new User('Berta', 'Berta@mail.com', 'qwerty');
   let user3 = new User('Chris Christofferson', 'Chris@mail.com', 'qwertz');
 
+  let [recipe1, recipe2, recipe3] = mockRecipes;
+
   // let users = [user1, user2, user3];
-  // console.log(users);
+  // console.log(user1);
+
+  // console.log(new Recipe(recipe1.title));
+  user1.addRecipe(recipe1.title, recipe1.versions[0]);
+  // user1.recipes[0].addVersion([]);
+  // user1.recipes[0].addVersion([]);
+  // user1.recipes[0].addVersion(recipe1.versions[0], recipe1.versions[0].ingredients);
+  console.log(user1.recipes[0]);
+
+  // console.log(user1.recipes[0]);
+
 
   // below now works
   // console.log(await UserService.add(user1));
@@ -26,12 +36,12 @@ async function main() {
   // TODO: problem seems to lie here
   // users.forEach((user) => UserService.add(user));
 
-  await UserService.add(user1);
-  await UserService.add(user2);
-  await UserService.add(user3);
-
-  const loadedUsers = await UserService.findAll();
-  console.log(loadedUsers);
+  // await UserService.add(user1);
+  // await UserService.add(user2);
+  // await UserService.add(user3);
+  //
+  // const loadedUsers = await UserService.findAll();
+  // console.log(loadedUsers);
 
   // addRecipe
 // console.log('======================================');
