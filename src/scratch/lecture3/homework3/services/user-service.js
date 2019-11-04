3fb1ea762eabc5f792eefc69b76a13a411e34196
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-04 13:10:37 +0100

    added a validation before a user is added to db

--- src/scratch/lecture3/homework3/services/user-service.js
+++ src/scratch/lecture3/homework3/services/user-service.js
@@ -1,9 +1,19 @@
+// import validator from 'validator';
+import isEmail from 'validator/lib/isEmail';
+
 const BaseService = require('./base-service');
 const UserModel = require('../models/user');
 
 class UserService extends BaseService {
   constructor() {
     super(UserModel, `${__dirname}/../user-database.json`);
+  }
+
+  async add(person) {
+    if (!isEmail(person.email)) {
+      return null;
+    }
+    return await super.add(person);
   }
 }
 
