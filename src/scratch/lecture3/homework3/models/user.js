26f1a56f7cec9562452ad3f0e48869a121a56ce0
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-05 16:15:30 +0100

    refactoring

--- src/scratch/lecture3/homework3/models/user.js
+++ src/scratch/lecture3/homework3/models/user.js
@@ -46,3 +46,83 @@ module.exports = class User {
     return user;
   }
 };
+
+// $ node test.js
+// [Function: Bibble]
+// private values
+// undefined
+// private funcs
+// undefined
+// undefined
+// public funcs def
+// undefined
+// undefined
+// public funcs call
+// PRIVATE!
+// 42
+//
+// ~~~~~~~~~~~~~~~~
+//
+// test.js
+//
+// ----- >% ---------- %< -----
+// const Bibble = require('./models/bibble');
+//
+// console.log(Bibble);
+//
+// const bibble = new Bibble();
+//
+// console.log('private values')
+// console.log(bibble._privateVal1);
+//
+// console.log('private funcs')
+// console.log(bibble._privateMethod1);
+// console.log(bibble._privateMethod2);
+//
+// console.log('public funcs def')
+// console.log(bibble._publicMethod1);
+// console.log(bibble._publicMethod2);
+//
+// console.log('public funcs call')
+// console.log(bibble.publicMethod1());
+// console.log(bibble.publicMethod2());
+// ----- >% ---------- %< -----
+//
+// models/bibble.js
+//
+// ----- >% ---------- %< -----
+// const Bibble = (function() {
+//   const _privateVal1 = 42;
+//
+//   const _privateMethod1 = function() {
+//     return 'PRIVATE!';
+//   };
+//
+//   const _privateMethod2 = function() {
+//     return _privateVal1;
+//   };
+//
+//   class Bibble {
+//     constructor() {}
+//
+//     publicMethod1() {
+//       return _privateMethod1();
+//     }
+//
+//     publicMethod2() {
+//       return _privateMethod2();
+//     }
+//   }
+//
+//   return Bibble;
+// })(); // <==== call it immediately to return a Bibble class
+//
+// /*
+// module.exports = {
+//   Bibble: Bibble,
+//   Babble: Babble,
+// };
+// */
+//
+// module.exports = Bibble;
+// ----- >% ---------- %< -----
