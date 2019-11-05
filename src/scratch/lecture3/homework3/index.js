390f2fd8ff7747e5981a4a04f2c8867698c4129d
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-05 16:04:58 +0100

    cleaned up file so it is easier to understand what's going on

--- src/scratch/lecture3/homework3/index.js
+++ src/scratch/lecture3/homework3/index.js
@@ -1,35 +1,39 @@
 /* eslint-disable */
 
 const User = require('./models/user');
-const Recipe = require('./models/recipe');
-
-// const RecipeService = require('./services/recipe-service');
 const UserService = require('./services/user-service');
-
 let mockRecipes = require('./models/recipes');
 
-console.log('The Eternal Menu!');
 console.log('============================================');
 
 async function main() {
-  let user1 = new User('Antonia', 'Antonia@mail.com', '1234');
+  // let [recipe1, recipe2, recipe3] = mockRecipes;
+
+  // CREATING USERS
+  // let user1 = new User('Antonia', 'Antonia@mail.com', '1234');
+  // let user2 = new User('Berta', 'Berta@mail.com', 'qwerty');
+  // let user3 = new User('Chris Christofferson', 'Chris@mail.com', 'qwertz');
+
+  // ADDING USERS to DATABASE
   // await UserService.add(user1);
-  // console.log(await UserService.findAll());
-  // const user1 = await UserService.findAll()[0];
+  // await UserService.add(user2);
+  // await UserService.add(user3);
 
-  let user2 = new User('Berta', 'Berta@mail.com', 'qwerty');
-  let user3 = new User('Chris Christofferson', 'Chris@mail.com', 'qwertz');
+  // RETRIEVING USERS FROM DATABASE
+  const [user1, user2, user3] = await UserService.findAll();
+  // console.log(user1, user2, user3);
 
-  let [recipe1, recipe2, recipe3] = mockRecipes;
-
-  // let users = [user1, user2, user3];
-  // console.log(user1);
-
+  // ADDING A RECIPE TO A USER
+  // CREATING A VERSION FOR A RECIPE
   const eggVersion1 = {
     cookingTime: 7,
     ingredients: [],
   };
+  // user1.saveRecipe('eggs', eggVersion1);
+  // await UserService.update(user1);
+  // console.log(user1);
 
+  // CREATING MORE VERSIONS FOR A RECIPE
   const eggVersion2 = {
     cookingTime: 8.5,
     ingredients: [],
@@ -45,6 +49,20 @@ async function main() {
     ingredients: [],
   };
 
+  // ADDING VERSIONS TO AN EXISTING RECIPE
+  const eggRecipe = user1.getRecipeById(user1.recipes[0].id);
+  // eggRecipe.saveVersion(eggVersion2);
+  // eggRecipe.saveVersion(eggVersion3);
+  // eggRecipe.saveVersion(eggVersion4);
+  // console.log(eggRecipe);
+  // await UserService.update(user1);
+
+  // DELETING A VERSION
+  // eggRecipe.deleteVersionById(3);
+  // console.log(eggRecipe);
+  // await UserService.update(user1);
+
+  // ADDING AN INGREDIENT
   const eggIngredients = [
     {
       amount: 1,
@@ -52,66 +70,14 @@ async function main() {
     },
   ];
 
-  // console.log('adding recipe ===========================');
-  user1.saveRecipe('eggs', eggVersion1);
-  // console.log(user1);
+  // SAVING AN INGREDIENT TO AN EXISTING VERSION
+  const eggRecipeVersion1 = eggRecipe.versions[0];
+  // eggRecipeVersion1.saveIngredients(eggIngredients);
+  // console.log(eggRecipeVersion1);
+  // await UserService.update(user1);
 
-  const eggRecipe = user1.getRecipeById(user1.recipes[0].id);
-  // console.log('version 2 ===========================');
-  eggRecipe.saveVersion(eggVersion2);
-  eggRecipe.saveVersion(eggVersion3);
-  eggRecipe.saveVersion(eggVersion4);
-
-  // console.log('delete version 2 ===========================');
-  // eggRecipe.deleteVersionById(3);
-
-  // console.log('save ingredients  ===========================');
-  const version1 = eggRecipe.versions[0];
-  version1.saveIngredients(eggIngredients);
-
-  await UserService.update(user1);
-
-  // eggRecipe.newVersion(2);
-
-  // console.log(eggRecipe.versions);
-
-  // below now works
-  // console.log(await UserService.add(user1));
-  // no await: returns a promise <pending>
-
-  // TODO: problem seems to lie here
-  // users.forEach((user) => UserService.add(user));
-
-  // await UserService.add(user1);
-  // await UserService.add(user2);
-  // await UserService.add(user3);
-  //
-  // const loadedUsers = await UserService.findAll();
-  // console.log(loadedUsers);
-
-  // addRecipe
-  // console.log('======================================');
-  // console.log(recipe1.versions[0]);
-  // user1.addRecipe(recipe1.title, recipe1.versions[0]);
-  // console.log(user1.recipes);
-
-  // const wtmb = new Meetup('Women Techmakers Berlin', 'Wayfair');
-  // armagan.attend(wtmb);
-  // mert.attend(wtmb);
-  // wtmb.report();
-  //
-  // await PersonService.add(mert);
-  // await PersonService.add(armagan);
-  //
-  // const people = await PersonService.findAll();
-  //
-  // console.log(people);
-  //
-  // await MeetupService.add(wtmb);
-  //
-  // const meetup = await MeetupService.find();
-  //
-  // meetup.report();
+  console.log(user1.recipes);
+  // console.log(user1, user2, user3);
 }
 
 main();
