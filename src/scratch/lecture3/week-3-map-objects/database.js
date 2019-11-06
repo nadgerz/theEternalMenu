3f59d9410793ee4a6a36f250afcee5853765e518
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-06 18:59:59 +0100

    removed all files tied to WTMB JS CC lectures

--- src/scratch/lecture3/week-3-map-objects/database.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-module.exports = {
-  save(data) {
-    fs.writeFileSync('data.json', JSON.stringify(data));
-  },
-  load() {
-    return JSON.parse(fs.readFileSync('data.json'));
-  },
-};
