d8a43ec2386ba790b5d8d5ec7ef79e3c8e43b7cf
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-28 21:49:45 +0100

    initial overview page layout

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import ReactDOM from 'react-dom';
 import Header from './components/Header';
 // import FormikLogin from './Login';
 // import RecipeForm from './RecipeForm';
+import {
+  CookingTimeIcon,
+  ServingSizeIcon,
+  HeartOutlineIcon,
+} from './assets/SVG/svg';
+
 import './assets/CSS/app.css';
 
 // import SearchParams from './SearchParams';
@@ -19,6 +25,49 @@ const App = () => {
       <Header />
 
       <main id={'main'}>
+        <div className={'recipes-and-filter'}>
+          <aside id={'recipe-filter'}>
+            <h2>Filter</h2>
+            <div className={'cooking-time'}>
+              <div className="filter-title">
+                <CookingTimeIcon />
+                <h6>Cooking Time</h6>
+              </div>
+              <div className="filter-slider"></div>
+            </div>
+            <div className={'serving-size'}>
+              <div className="filter-title">
+                <ServingSizeIcon />
+                <h6>Serving Size</h6>
+              </div>
+              <div className="filter-slider"></div>
+            </div>
+          </aside>
+          <article id="recipes">
+            <h2>
+              Recipes <span>{12} in total</span>
+            </h2>
+            <div className={'recipe-grid'}>
+              <div className="recipe-card add-recipe">
+                <h3>
+                  <a href="/">Add a Recipe</a>
+                </h3>
+              </div>
+              <div className="recipe-card">
+                <div className={'heart-tag'}>
+                  <div className={'heart-outline-icon'}>
+                    <HeartOutlineIcon />
+                  </div>
+                </div>
+                <div className={'recipe-card-title'}>
+                  <h5>A Recipe Title</h5>
+                  <div className="chevron"></div>
+                </div>
+                <div className={'recipe-thumb'}></div>
+              </div>
+            </div>
+          </article>
+        </div>
         {/*<Router>*/}
         {/*<SearchParams path={'/'}/>*/}
         {/*<Recipe path={'/details/:id'}/>*/}
