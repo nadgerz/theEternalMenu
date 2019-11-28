c6532c596f11e55a4d9cea1a788d8e45595b7042
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-11-28 12:25:06 +0100

    removing no longer need imports

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 // import { Router, Link } from '@reach/router';
 import { Link } from '@reach/router';
 // import FormikLogin from './Login';
-import RecipeForm from './RecipeForm';
+// import RecipeForm from './RecipeForm';
 import './assets/CSS/app.css';
 
 // import SearchParams from './SearchParams';
@@ -15,47 +15,22 @@ const App = () => {
     // will do nothing in production
     <React.StrictMode>
       <header>
-        <nav className="burger-menu">
-          <div className="bars">
-            <div className="bar"></div>
-            <div className="bar"></div>
-            <div className="bar"></div>
-          </div>
-        </nav>
+        <nav className="burger-menu"></nav>
         <div className="site-branding">
           <Link to={'/'}>
-            <h1>The Eternal Menu</h1>
+            <h1 className={'hidden'}>The Eternal Menu</h1>
           </Link>
         </div>
       </header>
 
       <main id={'main'}>
-        {/*<div className={'add-and-recents'}>*/}
-        {/*  <div className="add-new">*/}
-        {/*    <Link to="/">*/}
-        {/*      <div className="icon">+</div>*/}
-        {/*      Create New Recipe*/}
-        {/*    </Link>*/}
-        {/*  </div>*/}
-        {/*  */}
-        {/*  <div className="recents">*/}
-        {/*    <h3>RECENTLY EDITED</h3>*/}
-        {/*    <div className="recent-dishes">*/}
-        {/*      <div className="recent-dish"></div>*/}
-        {/*      <div className="recent-dish"></div>*/}
-        {/*      <div className="recent-dish"></div>*/}
-        {/*    </div>*/}
-        {/*  </div>*/}
-        {/*</div>*/}
         {/*<Router>*/}
         {/*<SearchParams path={'/'}/>*/}
         {/*<Recipe path={'/details/:id'}/>*/}
         {/*</Router>*/}
-
         {/*<FormikLogin />*/}
         {/*<FormikLogin email={'andrew@test.de'}/>*/}
-
-        <RecipeForm />
+        {/*<RecipeForm />*/}
       </main>
     </React.StrictMode>
   );
