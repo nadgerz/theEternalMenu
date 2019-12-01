c312d370bd65cfd3d4b9810333d9d4daa5a7b803
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-12-01 20:18:27 +0100

    using lifecycle methods to get data! Todo: update users/recipes

--- src/components/pages/Overview.js
+++ src/components/pages/Overview.js
@@ -1,45 +1,83 @@
 import React from 'react';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import axios from 'axios';
+// import config from 'config';
+// const serverPath = config.get('serverPath');
+const serverPath = 'http://localhost:3000';
 
 import AddRecipeCard from '../AddRecipeCard';
 import Filters from '../Filters';
 import RecipeCard from '../RecipeCard';
 
-import mockData from '../../assets/data/data';
-
 import '../../assets/CSS/pages/Overview.scss';
 
-const Overview = () => {
-  return (
-    <div id={'overview'} className={'recipes-and-filter'}>
-      {/* TODO: Filter will need to be passed props from user */}
-      <Filters />
+import mockData from '../../assets/data/data';
 
-      <article id="recipes" className={'user-recipes'}>
-        <h2>
-          Your Recipes <span>{mockData.user.recipes.length} in total</span>
-        </h2>
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
 
-        <div className={'recipe-grid'}>
-          <a href="/">
+    this.state = {
+      recipes: [],
+    };
+  }
+
+  componentDidMount() {
+    // this.getDataFromDbUsingFetch();
+    this.getDataFromDbUsingAxios();
+  }
+
+  // fetch data from database
+  getDataFromDbUsingFetch = () => {
+    fetch(`${serverPath}/user/all`)
+      .then(data => data.json())
+      .then(res => this.setState({ data: res.data }));
+  };
+
+  getDataFromDbUsingAxios = async () => {
+    try {
+      const result = await axios.get(`${serverPath}/user/all`);
+      console.log(result);
+      this.setState({ data: result });
+    } catch (err) {
+      console.log(err.response.data.message);
+    }
+  };
+
+  render() {
+    // const { recipes } = this.state;
+
+    return (
+      <div id={'overview'} className={'recipes-and-filter'}>
+        {/* TODO: Filter will need to be passed props from user */}
+        <Filters />
+
+        <article id="recipes" className={'user-recipes'}>
+          <h2>
+            Your Recipes <span>{mockData.user.recipes.length} in total</span>
+          </h2>
+
+          <div className={'recipe-grid'}>
             <AddRecipeCard />
-          </a>
 
-          {mockData.user.recipes.map((recipe, index) => {
-            return (
-              <RecipeCard
-                img={recipe.img}
-                title={recipe.title}
-                favourite={recipe.favourite}
-                key={
-                  recipe.title.toLowerCase().replace(/\s/g, '') + '_' + index
-                }
-              />
-            );
-          })}
-        </div>
-      </article>
-    </div>
-  );
-};
+            {mockData.user.recipes.map((recipe, index) => {
+              return (
+                <RecipeCard
+                  img={recipe.img}
+                  title={recipe.title}
+                  favourite={recipe.favourite}
+                  key={
+                    recipe.title.toLowerCase().replace(/\s/g, '') + '_' + index
+                  }
+                />
+              );
+            })}
+          </div>
+        </article>
+      </div>
+    );
+  }
+}
 
 export default Overview;
