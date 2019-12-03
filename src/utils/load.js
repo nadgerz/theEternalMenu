c4fe9044a3d573445cd21b0bdffcc7ab0b317859
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-12-03 21:27:23 +0100

    use faker

--- src/utils/load.js
+++ src/utils/load.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+const faker = require('faker');
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
@@ -7,16 +8,49 @@ const log = console.log;
 const connectDB = require('../../config/db');
 connectDB();
 
+function randomBetween(min, max) {
+  return ~~(Math.random() * (max - min)) + min;
+}
+
 const RecipeSchema = Schema({
-  title: {
-    type: String,
-  },
+  title: String,
   servingSize: Number,
   cookingTime: Number,
+  image: String,
 });
 
 const Recipe = mongoose.model('recipe', RecipeSchema);
 // console.log(Recipe);
+
+Recipe.collection.drop();
+
+let debug;
+debug = true;
+debug = false;
+
+const MAX_PROMISES = 100;
+
+let promises = [];
+
+for (let i = 0; i < MAX_PROMISES; i++) {
+  const recipe = {
+    title: faker.random.words(),
+    cookingTime: randomBetween(5, 120),
+    servingSize: randomBetween(1, 12),
+    image: faker.image.food(),
+  };
+  const r = new Recipe(recipe);
+  const p = r
+    .save()
+    .then(rec => {
+      if (debug) {
+        console.log('Saved', rec);
+      }
+    })
+    .catch(err => console.error(err.message));
+
+  promises.push(p);
+}
 
 const recipe1 = {
   title: 'Eggs Benny',
@@ -41,19 +75,33 @@ const r2 = new Recipe(recipe2);
 const r3 = new Recipe(recipe3);
 // console.log(r1)
 
-Recipe.collection.drop();
-
+/*
 const p1 = r1
   .save()
-  .then(rec => console.log('Saved', rec))
+  .then(rec => {
+    if (debug) {
+      console.log('Saved', rec);
+    }
+  })
   .catch(err => console.error(err.message));
 const p2 = r2
   .save()
-  .then(rec => console.log('Saved', rec))
+  .then(rec => {
+    if (debug) {
+      console.log('Saved', rec);
+    }
+  })
   .catch(err => console.error(err.message));
 const p3 = r3
   .save()
-  .then(rec => console.log('Saved', rec))
+  .then(rec => {
+    if (debug) {
+      console.log('Saved', rec);
+    }
+  })
   .catch(err => console.error(err.message));
 
 Promise.all([p1, p2, p3]).then(() => process.exit(0));
+*/
+
+Promise.all(promises).then(() => process.exit(0));
