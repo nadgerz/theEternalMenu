a5df33f44ecc7e52174b7a735681945c425e1749
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-12-17 18:56:19 +0100

    Brian Holt's React V5: before generizising the dropdown menus

--- frontend/src/components/SearchParams.js
+++ frontend/src/components/SearchParams.js
@@ -1,9 +1,67 @@
 import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
+
+import useDropdown from './useDropdown';
+
 
 const SearchParams = () => {
+  const [location, setLocation] = useState('Seattle, WA');
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
+
   return (
-    <div>
-      'hello'
+    <div className={'searc-params'}>
+      <form action="">
+        <label htmlFor="location">
+          Location
+          <input id="location"
+                 value={location}
+                 onChange={event => setLocation(event.target.value)}
+                 placeholder={'Location'}
+                 type="text"
+          />
+          <button>Submit</button>
+        </label>
+        <br/>
+        <label htmlFor="animal">
+          Animal
+          <select id={'animal'}
+                  value={animal}
+                  onChange={event => setAnimal(event.target.value)}
+                  onBlur={event => setAnimal(event.target.value)}
+          >
+            <option value="all">All</option>
+            {
+              ANIMALS.map(animal =>
+                <option key={animal}
+                        value={animal}
+                >{animal}</option>,
+              )
+            }
+          </select>
+        </label>
+        <br/>
+        <label htmlFor="breed">
+          Breed
+          {/* is disabled if no breeds available */}
+          <select id={'breed'}
+                  disabled={!breeds.length}
+                  onChange={event => setBreed(event.target.value)}
+                  onBlur={event => setBreed(event.target.value)}
+                  value={breed}
+          >
+            <option value="all">All</option>
+            {
+              breeds.map(breedString =>
+                <option key={breedString}
+                        value={breedString}
+                >{breedString}</option>,
+              )
+            }
+          </select>
+        </label>
+      </form>
+
     </div>
   );
 };
