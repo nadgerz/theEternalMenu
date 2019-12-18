0d8e771dedb275060feddf1365cc623967e85b3b
Kerstin Dengl <kerstin.dengl@gmail.com>
2019-12-18 19:09:50 +0100

    new file for useful component snippets

--- frontend/src/utils/snippets.js
+++ frontend/src/utils/snippets.js
@@ -1,23 +1,10 @@
 import React from 'react';
 
-export default Pet = props => {
-  const { name, animal, breed, media, location, id } = props;
-
-  let hero = 'http://placecorgi.com/300/300';
-  if (media.length) {
-    hero = media[0].small;
-  }
-
+export const DebugDump = props => {
   return (
-    <a href={`/details/${id}`} className="pet">
-      <div className="image-container">
-        <img src={hero} alt={name} />
-      </div>
-      <div className="info">
-        <h1>{name}</h1>
-        <h2>{`${animal} — ${breed} — ${location}`}</h2>
-      </div>
-    </a>
+    <pre>
+      <code>{JSON.stringify(props, null, 2)}</code>
+  </pre>
   );
 };
 
